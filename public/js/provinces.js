94d75d79bc2657b767fdadad06240b962dbf1430
Michael Martinez <xn1p3r@gmail.com>
2019-08-06 15:36:03 -0600

    Cambios en js de carga de provincias

--- public/js/provinces.js
+++ public/js/provinces.js
@@ -1,8 +1,8 @@
 'use strict'
 
-const provinces = get_Provinces();
+const provinces = getProvinces();
 
-function get_Provinces() {
+function getProvinces() {
   let provinces = "";
   let request = $.ajax({
     url: '/api/province',
@@ -25,41 +25,41 @@ function get_Provinces() {
   return provinces;
 };
 
-const select_provinces = document.querySelector('#sltProvince');
-const select_counties = document.querySelector('#sltCounty');
-const select_districts = document.querySelector('#sltDistrict');
+const selectProvinces = document.querySelector('#sltProvince');
+const selectCounties = document.querySelector('#sltCounty');
+const selectDistricts = document.querySelector('#sltDistrict');
 
-let add_Provinces = () => {
+let addProvinces = () => {
   let blankOption = new Option('');
+
   blankOption.value = '';
   blankOption.selected = true;
-  select_provinces.appendChild(blankOption);
+  selectProvinces.appendChild(blankOption);
 
-  for (let i = 0; i < provinces.length; i++) {
+  for (let i=0; i<provinces.length; i++) {
     let newOption = new Option(provinces[i]['name']);
     
-    newOption.value = provinces[i]['_id'];
-    select_provinces.appendChild(newOption);
+    newOption.value = provinces[i]['name'];
+    selectProvinces.appendChild(newOption);
   }
 };
 
-let add_Counties = () => {
-  let province = select_provinces.value;
-  
-  select_counties.innerHTML = '';
+let addCounties = () => {
+  let province = selectProvinces.value;
+  selectCounties.innerHTML = '';
 
   let blankOption = new Option('');
   blankOption.value = '';
-  select_counties.appendChild(blankOption);
+  selectCounties.appendChild(blankOption);
 
-  for (let i = 0; i < provinces.length; i++) {
-    if (province == provinces[i]['_id']) {
+  for (let i=0; i<provinces.length; i++) {
+    if (province == provinces[i]['name']) {
       let counties = provinces[i]['counties'];
       
       for (i = 0; i < counties.length; i++) {
         let newOption = new Option(counties[i]['name']);
         newOption.value = counties[i]['name'];
-        select_counties.appendChild(newOption);
+        selectCounties.appendChild(newOption);
       }
 
       break;
@@ -67,18 +67,18 @@ let add_Counties = () => {
   }
 }
 
-let add_Districts = () => {
-  let province = select_provinces.value;
-  let county = select_counties.value;
-  let counties = "";
+let addDistricts = () => {
+  let province = selectProvinces.value;
+  let county = selectCounties.value;
+  let counties = '';
 
-  select_districts.innerHTML = '';
+  selectDistricts.innerHTML = '';
 
   let blankOption = new Option('');
   blankOption.value = '';
-  select_districts.appendChild(blankOption);
+  selectDistricts.appendChild(blankOption);
 
-  for (let i = 0; i < provinces.length; i++) {
+  for (let i=0; i<provinces.length; i++) {
     if (province == provinces[i]['name']) {
       counties = provinces[i]['counties'];
       break;
@@ -86,21 +86,20 @@ let add_Districts = () => {
   }
 
   for (let i = 0; i < counties.length; i++) {
-    if (county == counties[i]['_id']) {
+    if (county == counties[i]['name']) {
       let districts = counties[i]['districts'];
       
       for (let i = 0; i < districts.length; i++) {
         let newOption = new Option(districts[i]['name']);
        
-        newOption.value = districts[i]['name  '];
-        select_districts.appendChild(newOption);
+        newOption.value = districts[i]['name'];
+        selectDistricts.appendChild(newOption);
       }
     }
   }
 }
 
-select_provinces.addEventListener('change', add_Counties);
-select_provinces.addEventListener('change', add_Districts);
-select_counties.addEventListener('change', add_Districts);
+selectProvinces.addEventListener('change', addCounties);
+selectCounties.addEventListener('change', addDistricts);
 
-add_Provinces(); 
+addProvinces(); 
