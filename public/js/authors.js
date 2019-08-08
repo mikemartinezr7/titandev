5c3a4ae6e4ada2b3d404af7eb1cd81a68e229a58
Alisson Ruiz Navarro <rvillaltan@ucenfotec.ac.cr>
2019-08-08 13:58:23 -0600

    update to authors

--- public/js/authors.js
+++ public/js/authors.js
@@ -25,6 +25,18 @@ function isValidData() {
     lastname.classList.remove('error');
   }
 
+  if (biography.value == '') {
+    isValid = false;
+    biography.classList.add('error');
+  } else {
+    biography.classList.remove('error');
+  }
+  if (birthyear.value == '') {
+    isValid = false;
+    birthyear.classList.add('error');
+  } else {
+    birthyear.classList.remove('error');
+  }
   return isValid;
 }
 
@@ -36,24 +48,84 @@ function createAuthors(event) {
   const biography = document.getElementById('biography').value;
   const birthyear = document.getElementById('birthyear').value;
 
-  if (isValidData()) {
-  let request = $.ajax({
-    url: '/api/author',
-    method: 'POST',
-    data: {
-      firstname: firstname,
-      lastname: lastname,
-      biography: biography,
-      birthyear: birthyear
-    },
-    dataType: 'json',
-    contentType: 'application/x-www-form-urlencoded; charset=UTF-8'
-  }).done(function(data) {
-    console.log('Done' + data.success);
-  }).fail(function(error) {
-    console.log('Fail ' + error);
-  });
+
+    if(isValidData()){
+      let request = $.ajax({
+        url: 'http://localhost:3000/api/author',
+        method: 'POST',
+        data: {
+          firstname: firstname,
+          lastname: lastname,
+          biography: biography,
+          birthyear: birthyear
+        },
+        dataType: 'json',
+        contentType: 'application/x-www-form-urlencoded; charset=UTF-8'
+      }).done(function (data) {
+        console.log('Done' + data.success);
+        swal({
+          title:'exito',
+          text:"Author guardado",
+          type:'success'
+          }).then(function() {
+            window.location = "../index.html";
+        })
+
+      }).fail(function (error) {
+        console.log('Fail ' + error);
+      });
+      
 } else {
-  console.log('Erro sus datos son invalidos');
+  console.log('Error sus datos son invalidos');
 }
 }
+
+let list_authors = () => { //controlador
+  let author = [];
+  let request = $.ajax({
+    url: "http://localhost:3000/api/book",
+    method: "GET",
+    data: {
+    },
+    dataType: "json",
+    contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+    async : false
+  });
+
+  request.done(function (res) {
+      author = res.author;
+    
+  });
+
+  request.fail(function (jqXHR, textStatus) {
+    
+  });
+  return author;
+ 
+};
+
+let search_authors = (id_autor) => {
+  let authorFound = [];
+
+  let request = $.ajax({
+    url: "http://localhost:3000/api/authors"+ id_autor,
+    method: "GET",
+    data: {
+    },
+    dataType: "json",
+    contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+    async : false
+  });
+
+  request.done(function (res) {
+      authorFound = res.authorFound;
+    
+  });
+
+  request.fail(function (jqXHR, textStatus) {
+    
+  });
+  return authorFound;
+ 
+};
+
