edb61831ff89d72eb65e0b3638c978fd4368e73e
Michael Martinez <xn1p3r@gmail.com>
2019-08-12 22:36:53 -0600

    Cambios en index y layout principal

--- public/js/layout.js
+++ public/js/layout.js
@@ -1,15 +1,14 @@
-$('.left-menu-toggle').click(function () {
-  if ($('#leftmenu').hasClass('left-menu-open')) {
-    $('#leftmenu').addClass('left-menu-close');
-    $('#leftmenu').removeClass('left-menu-open');
-    $('.left-menu-toggle').html('>>');
-    $('.left-menu-item-text').hide();1
-  } else if ($('#leftmenu').hasClass('left-menu-close')) {
-    $('#leftmenu').addClass('left-menu-open');
-    $('#leftmenu').removeClass('left-menu-close');
-    $('.left-menu-toggle').html('<<');
-    $('.left-menu-item-text').show();
-  }
+$('#leftmenu').mouseenter(function () {
+  $('#leftmenu').addClass('left-menu-open');
+  $('#leftmenu').removeClass('left-menu-close');
+  $('.left-menu-toggle').html('<<');
+  $('.left-menu-item-text').show();
+  $('#leftmenu').addClass('left-menu-open');
+}).mouseleave(function () {
+  $('#leftmenu').addClass('left-menu-close');
+  $('#leftmenu').removeClass('left-menu-open');
+  $('.left-menu-toggle').html('>>');
+  $('.left-menu-item-text').hide();
 });
 
 $(window).on('load', function () {
@@ -18,24 +17,13 @@ $(window).on('load', function () {
   $('#leftmenu, #content').height($(window).height() - $('#topmenu-container').height())
 });
 
-$('.notification-close').click(function () {
-  $(this).parent().hide();
-});
-
 function showNotification(message, type) {
-  let style = '';
-
-  switch (type) {
-    case 'success': style = '.success'
-    case 'danger' : style = '.danger'
-    default: style = '.danger';
-  }  
-
   let info = {
     message : message,
-    type: style
+    type: type
   }
   
   $('#notifications').empty();
   $('#notificationTemplate').tmpl(info).appendTo('#notifications');
+  console.log(info);
 }
