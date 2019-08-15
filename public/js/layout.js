3059d5a0c1cb7d6f76d4395b4f11179243a1718c
Michael Martinez <xn1p3r@gmail.com>
2019-08-14 19:03:09 -0600

    Cambios en layout del sitio y formularios

--- public/js/layout.js
+++ public/js/layout.js
@@ -1,13 +1,10 @@
 $('#leftmenu').mouseenter(function () {
   $('#leftmenu').addClass('left-menu-open');
   $('#leftmenu').removeClass('left-menu-close');
-  $('.left-menu-toggle').html('<<');
   $('.left-menu-item-text').show();
-  $('#leftmenu').addClass('left-menu-open');
 }).mouseleave(function () {
   $('#leftmenu').addClass('left-menu-close');
   $('#leftmenu').removeClass('left-menu-open');
-  $('.left-menu-toggle').html('>>');
   $('.left-menu-item-text').hide();
 });
 
