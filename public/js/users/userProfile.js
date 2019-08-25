e86f7e04480fab67abdf11c23b03d0bcc458b63e
unknown <gsanchezm@ucenfotec.ac.cr>
2019-08-25 10:51:46 -0600

    usuario

--- public/js/users/userProfile.js
+++ public/js/users/userProfile.js
@@ -27,11 +27,33 @@ let user = getUser();
 
 console.log(user)
 
+user.idType = user.idType.charAt(0).toUpperCase()+user.idType.slice(1)
+
+switch (user.gender){
+    case 'M': user.gender = 'Masculino'; break;
+    case 'F': user.gender = 'Femenino'; break;
+    case 'O': user.gender = 'Otro'; break;
+    case 'P': user.gender = 'Prefiero no decir'; break;
+    default: break;
+}
+
 if (user.exchange){
     user.exchange = "Sí"
 }else{
     user.exchange = "No"
 }
+
+let favGenres ='';
+
+for(let i=0;i<user.favoriteGenres.length;i++){
+    if(i==user.favoriteGenres.length-1){
+        favGenres+=user.favoriteGenres[i]["name"]
+    }else{
+        favGenres+=user.favoriteGenres[i]["name"]+", "
+    }
+}
+
+user.favoriteGenres = favGenres
 
 $('#user').empty();
 $('#userTemplate').tmpl(user).appendTo('#user');
